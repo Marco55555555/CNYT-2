26237602ddc2527c1b39d9e87b888c24b97ff5a9
Marco55555555 <142811404+Marco55555555@users.noreply.github.com>
2023-09-06 19:23:58 -0500

    Update TAREA2.py

--- TAREA2.py
+++ TAREA2.py
@@ -105,3 +105,91 @@ def ConjM(Data1):
             columna.append(conj(Data1[i][j]))
         res.append(columna)
     return res
+    #########################################################################
+
+def AdjM(Data1):
+    res = []
+    for i in range(len(Data1)):
+        columna = []
+        for j in range(len(Data1[i])):
+            columna.append(.conj(Data1[j][i]))
+        res.append(columna)
+    return res
+
+def AdjV(Data1):
+    res = []
+    for i in range(len(Data1)):
+        res.append(conj(Data1[i]))
+    return res
+    
+def MulM(Data1,Data2):
+    Matriz = []
+    for i in range(len(Data1)):
+        Fila = []
+        for j in range(len(Data2[0])):
+            Rp = 0
+            Ip = 0
+            for k in range(len(Data1[0])):
+                Rp += pro(Data1[i][k],Data2[k][j])[0]
+                Ip += pro(Data1[i][k],Data2[k][j])[1]
+            Fila.append((Rp,Ip))
+        Matriz.append(Fila)
+    return Matriz
+
+def AcMV(Data1,Data2):
+    res=[]
+    for i in range(len(Data1)):
+        Rp = 0
+        Ip = 0
+        for k in range(len(Data2)):
+            Rp += pro(Data1[i][k],Data2[k])[0]
+            Ip += pro(Data1[i][k],Data2[k])[1]
+        res.append((Rp,Ip))
+    return res
+
+def PriV(Data1,Data2):
+    Data1 = AdjV(Data1)
+    Rp = 0
+    Ip = 0
+    for i in range(len(Data1)):
+        Rp += pro(Data1[i],Data2[i])[0]
+        Ip += pro(Data1[i],Data2[i])[1]
+    return (Rp,Ip)
+
+def NorV(Data1):
+    res=0
+    for i in range(len(Data1)):
+        res += Data1[i][0]**2+Data1[i][1]**2
+    return math.sqrt(res)
+
+def DisV(Data1,Data2):
+    res = []
+    for i in range(len(Data1)):
+        res.append(res(Data1[i],Data2[i]))
+    d = 0
+    for j in range(len(res)):
+        d += mod(res[j])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
